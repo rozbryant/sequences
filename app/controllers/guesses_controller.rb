ff2a0cfc9c6d43c660965e348614cdc2e7ad7f0a
Raghu Betina <rbetina@gmail.com>
2016-10-25 21:01:27 -0500

    Added database

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -2,6 +2,13 @@ class GuessesController < ApplicationController
   def index
     # params = {"first_number"=>"4", "second_number"=>"5", "third_number"=>"6"}
 
+    g = Guess.new
+    g.first_num = params["first_number"]
+    g.second_num = params["second_number"]
+    g.third_num = params["third_number"]
+    g.save
+
+    @list = Guess.all
 
     render("guesses/index.html.erb")
   end
